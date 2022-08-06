49d839139373e8268e6140b372c076acf86b7f8c
Anand Menon <anandsm08@gmail.com>
2022-08-07 01:04:50 +0530

    second commit/optioncards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:walletapp/util/my_wallet.dart';
+import 'package:walletapp/util/option_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +13,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //pagecontroller
+  final _pagecontroller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +46,9 @@ class _HomePageState extends State<HomePage> {
             // ignore: sized_box_for_whitespace
             Container(
                 height: 170,
-                child: PageView(scrollDirection: Axis.horizontal,
+                child: PageView(
+                    scrollDirection: Axis.horizontal,
+                    controller: _pagecontroller,
                     // ignore: prefer_const_literals_to_create_immutables
                     children: [
                       MyWallet(
@@ -59,7 +65,38 @@ class _HomePageState extends State<HomePage> {
                           color: Colors.purple[400])
                     ])),
             //varbuttons
-
+            SizedBox(
+              height: 10,
+            ),
+            SmoothPageIndicator(
+              controller: _pagecontroller,
+              count: 3,
+              effect: SwapEffect(
+                  activeDotColor: Colors.grey.shade800,
+                  dotHeight: 5,
+                  dotWidth: 7),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    OptionCard(
+                        optioncolor: Colors.green[600],
+                        optionicon:
+                            'lib/walletappicons/icons8-cash-in-hand-48.png',
+                        optionname: 'Pay'),
+                    OptionCard(
+                        optioncolor: Colors.red[600],
+                        optionicon: 'lib/walletappicons/icons8-invoice-48.png',
+                        optionname: 'Bills'),
+                    OptionCard(
+                        optioncolor: Colors.teal[600],
+                        optionicon: 'lib/walletappicons/icons8-receive-64.png',
+                        optionname: 'Receive'),
+                  ]),
+            )
             //column
           ],
         ),
---
+++ lib/util/option_card.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables
+
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class OptionCard extends StatelessWidget {
+  final optionicon;
+  final optionname;
+  final optioncolor;
+  const OptionCard({
+    Key? key,
+    required this.optioncolor,
+    required this.optionicon,
+    required this.optionname,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      width: 120,
+      decoration: BoxDecoration(
+          color: optioncolor, borderRadius: BorderRadius.circular(25)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              height: 80,
+              width: 80,
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(70)),
+              child: Center(
+                child: Image.asset(optionicon),
+              )),
+          SizedBox(height: 5),
+          Text(
+            optionname,
+            style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[350],
+                fontWeight: FontWeight.w700),
+          )
+        ],
+      ),
+    );
+  }
+}
